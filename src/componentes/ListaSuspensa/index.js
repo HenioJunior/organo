b60cc21cb3e46de93af26ad94f7cf90caac74b5d
Henio Junior <henioajunior@gmail.com>
2022-08-05 17:01:41 -0700

    Controlando o estado

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,15 +1,18 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    console.log(props.itens)
-    
     return (
-    <div className='lista-suspensa'>
-        <label>{props.label}</label>
-        <select required={props.obrigatorio}>
-            {props.itens.map(item => <option key={item}>{item}</option>)}
-        </select>
-    </div>
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select
+                onChange={evento => props.aoAlterado(evento.target.value)}
+                required={props.obrigatorio}
+                value={props.value}>
+                    {props.itens.map(item => {
+                        return <option key={item}>{item}</option>
+                    })}
+            </select>
+        </div>
     )
 }
 export default ListaSuspensa
