efc9dc3cb84940c98aadc28e6c015327ec610ca4
Henio Junior <henioajunior@gmail.com>
2022-08-08 08:36:46 -0300

    Corrigindo bugs

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -7,10 +7,12 @@ const ListaSuspensa = (props) => {
             <select
                 onChange={evento => props.aoAlterado(evento.target.value)}
                 required={props.obrigatorio}
-                value={props.value}>
-                    {props.itens.map(item => {
-                        return <option key={item}>{item}</option>
-                    })}
+                value={props.valor}
+            >
+                <option value=""></option>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
             </select>
         </div>
     )
